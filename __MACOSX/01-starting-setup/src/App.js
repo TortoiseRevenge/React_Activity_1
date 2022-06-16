545729a9de35b2c9d3b8e6c4b3ec6d870a781177
Adithya <asunderesan2020@gmail.com>
2022-06-16 14:39:17 -0500

    stuff

--- __MACOSX/01-starting-setup/src/App.js
+++ __MACOSX/01-starting-setup/src/App.js
@@ -24,11 +24,11 @@ function App() {
     },
   ];
   return (
-    <div>
+    <div>        
       <ExpenseItem title={expenses[0].title}
       amount={expenses[0].amount}
       date={expenses[0].date}></ExpenseItem>
-
+      
       <ExpenseItem title={expenses[1].title}
       amount={expenses[1].amount}
       date={expenses[1].date}></ExpenseItem>
