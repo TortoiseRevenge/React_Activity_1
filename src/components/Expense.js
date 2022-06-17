cfcb68d8fdd615441e3c9aa87778c91ff1eee4ed
SeanR <s.m.reagan22@gmail.com>
2022-06-17 11:04:32 -0500

    Working Code

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -4,7 +4,7 @@ import ExpenseItem from "./ExpenseItem";
 import Card from './Card';
 function Expense(props){
     return(
-        <Card>
+        <Card className="expenses">
 <ExpenseItem title={props.items[0].title} 
       amount={props.items[0].amount}
       date  ={props.items[0].date}></ExpenseItem>
