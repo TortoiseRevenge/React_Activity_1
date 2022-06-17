6d6d4dd2fdd985e114b89ebd60696292edc3ead1
SeanR <s.m.reagan22@gmail.com>
2022-06-17 10:56:03 -0500

    Auto stash before merge of "Sean" and "origin/Adam3"

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,9 +1,10 @@
 
 import './Expense.css';
 import ExpenseItem from "./ExpenseItem";
+import Card from './Card';
 function Expense(props){
     return(
-        <div>
+        <Card>
 <ExpenseItem title={props.items[0].title} 
       amount={props.items[0].amount}
       date  ={props.items[0].date}></ExpenseItem>
@@ -16,7 +17,7 @@ function Expense(props){
       <ExpenseItem title={props.items[3].title} 
       amount={props.items[3].amount}
       date  ={props.items[3].date}></ExpenseItem>
-      </div>
+      </Card>
     );
 }
 export default Expense
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
-
+import Card from './Card';
 function ExpenseItem(props){
 
 return(
 <div>
-<div className='expense-item'>
+<Card className='expense-item'>
     <ExpenseDate date = {props.date}></ExpenseDate>
 <div className='expense-item__description'>
     <h2>{props.title}</h2>
@@ -14,7 +14,7 @@ return(
 <div className='expense-item__price'>
     ${props.amount}
 </div>
-</div>
+</Card>
 </div>
 );
 }
